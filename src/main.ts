12e729f262a21366b01fe3d91c7aed835012127e
ammar-bin-quddus <ammarcghs372@gmail.com>
2026-05-17 23:00:20 +0600

    seed medicine data

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,26 @@ import { setupSwagger } from './common/swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOrigins = Array.from(
+    new Set(
+      [
+        process.env.CORS_ORIGIN,
+        process.env.APP_URL,
+        'http://localhost:3000',
+      ]
+        .flatMap((value) => value?.split(',') ?? [])
+        .map((value) => value.trim())
+        .filter(Boolean),
+    ),
+  );
+
+  app.enableCors({
+    origin: corsOrigins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
